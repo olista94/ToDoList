49ceb22dee3fe4ecb48f7b7837b28f2cf7263ec0
afmontero <33687152+afmontero@users.noreply.github.com>
2019-01-13 17:30:11 +0100

    Mierdas para EDGE
    Cuando tengais un ratito probadlo

--- Views/Footer.php
+++ Views/Footer.php
@@ -1,7 +1,7 @@
 <div class="footer" id="footer">
-		<li><a><?php echo $strings['Creado por']; ?>: yq5lj9 </a></li>
-		<li><a><?php echo $strings['Fecha']; ?>: 30/11/2018</a></li>
-	</div>
+	<li><a><?php echo $strings['Creado por']; ?>: Anónimo </a></li>
+	<li><a><?php echo $strings['Fecha']; ?>: 30/12/2018</a></li>
+</div>
 </body>
 </html>
 
