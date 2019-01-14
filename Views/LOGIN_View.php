f01ce7eb330ded43ff4dbf372ec8e95095310ebe
afmontero <33687152+afmontero@users.noreply.github.com>
2019-01-14 17:56:04 +0100

    Banderitas responsive en el login tambien

--- Views/LOGIN_View.php
+++ Views/LOGIN_View.php
@@ -19,30 +19,13 @@
 			
 	<div class="welcome">		
 	<img src="../img/logo.png" alt="Logo" width="60" height="60"> <h1>ToDoList</h1>
-	</div>	
-
-			<div class="flags1" style="margin-right:420px; margin-top:270px;" style="position:absolute">
-				<form name='idioma' action="../Functions/CambioIdioma.php" method="POST" style="display: inline-block; margin:10; padding:0;">
-					<input type="hidden" name='idioma' value="ENGLISH">
-					<input type="image" src="../img/uk.png"  width="45px">
-				</form>
-				
-				<form name='idioma' action="../Functions/CambioIdioma.php" method="POST" style="display: inline-block; margin:10; padding:0;">
-					<input type="hidden" name='idioma' value="SPANISH" >
-					<input type="image"  src="../img/spain.png"  width="35px" >
-				</form>
-				
-				<form name='idioma' action="../Functions/CambioIdioma.php" method="POST" style="display: inline-block; margin:10;  padding:0;">
-					<input type="hidden" name='idioma' value="GALLAECIAN" >
-					<input type="image"  src="../img/galicia.png" width="35px">	
-				</form>
-			</div>
+	</div>
 			
 	<form class="login_form" name = 'FormLogin' action='../Controllers/Login_Controller.php' method='post'>
 			
 		<div>	
 			<legend><?php echo $strings['Inicia sesión']; ?>
-			<button style="margin-right:-150px" type="submit" title="<?php echo $strings['Registrar nuevo usuario']; ?>" class="registrarse" name="action" value="Confirmar_REGISTRO"></button>
+			<button type="submit" title="<?php echo $strings['Registrar nuevo usuario']; ?>" class="registrarse" name="action" value="Confirmar_REGISTRO"></button>
 			</legend>
 
 			<label for="login"><?php echo $strings['Login']; ?></label>
@@ -55,7 +38,24 @@
 		
 		<button type="submit" title="<?php echo $strings['Iniciar sesion']; ?>" value="Confirmar_LOGIN" name="action" class="aceptar" onclick="if (validarLogin(document.forms['FormLogin'])) document.forms['FormLogin'].submit();else return false;"></button>
 				
-	</form>							
+	</form>
+	<div class="flags1">
+		<form name='idioma' action="../Functions/CambioIdioma.php" method="POST" style="display: inline-block; margin:10; padding:0;">
+			<input type="hidden" name='idioma' value="ENGLISH">
+			<input type="image" src="../img/uk.png"  width="45px">
+		</form>
+		
+		<form name='idioma' action="../Functions/CambioIdioma.php" method="POST" style="display: inline-block; margin:10; padding:0;">
+			<input type="hidden" name='idioma' value="SPANISH" >
+			<input type="image"  src="../img/spain.png"  width="35px" >
+		</form>
+		
+		<form name='idioma' action="../Functions/CambioIdioma.php" method="POST" style="display: inline-block; margin:10;  padding:0;">
+			<input type="hidden" name='idioma' value="GALLAECIAN" >
+			<input type="image"  src="../img/galicia.png" width="35px">	
+		</form>
+	</div>
+						
 <?php
 			include '../Views/Footer.php';
 		} //fin metodo render
