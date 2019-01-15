da789603396c420257bcd7f7e3e4e6cd63dd0fb6
Iagosevic <micromachine13@hotmail.com>
2019-01-15 15:51:34 +0100

    Install e index comenmierdas

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
+#Comando para crear la BD
 mysql -uroot -piu -e "CREATE DATABASE todolist;";
+#Comando para los privilegios en la BD
 mysql -uroot -piu -e "GRANT ALL PRIVILEGES ON todolist.* TO todolist@localhost IDENTIFIED BY 'todolist';";
+#Indicamos donde esta el archivo sql en la carpeta del proyecto
 mysql -uroot -piu < ./Models/todolist.sql;
+#Todos los permisos en el directorio Files
 chmod -R 777 ./Files
