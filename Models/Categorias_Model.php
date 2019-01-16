eac4ba398ed4c4e1776d1670b4f076fefcf53667
afmontero <33687152+afmontero@users.noreply.github.com>
2019-01-16 17:28:45 +0100

    Organizado

---
+++ Models/Categorias_Model.php
@@ -0,0 +1,153 @@
+<!---MODELO DE LAS CATEGORIAS
+ CREADO POR los Cangrejas EL 21/12/2018-->
+<?php
+class CATEGORIAS_Model {
+	//Id de la categoria
+	var $id_CATEGORIAS;
+	//Nombre de la categoria
+	var $nombre;
+	
+//Constructor de la clase
+function __construct($id_CATEGORIAS,$nombre){
+	$this->id_CATEGORIAS = $id_CATEGORIAS;
+	$this->nombre = $nombre;
+	//Incluimos el archivo de acceso a la bd
+	include_once 'Access_DB.php';
+	//Funcion de conexion a la bd
+	$this->mysqli = ConnectDB();
+}
+
+//Funcion para insertar categorias
+function add(){
+			//Sentencia sql que insetara la categoria
+		$sql = "INSERT INTO categorias (
+			nombre
+			) 
+				VALUES (
+					'$this->nombre'
+					)";
+			
+			//Si ya se han insertado la PK o FK
+		if (!$this->mysqli->query($sql)) {
+			
+			return 'Error al insertar';
+		}
+		//operacion de insertado correcta
+		else{
+			return  'Insercion correcta'; 
+		}		
+	}
+//Funcion que obtiene todos los datos de una categoria especifica	
+	function rellenadatos() 
+{	
+    $sql = "SELECT * FROM categorias WHERE (`id_CATEGORIAS` = '$this->id_CATEGORIAS')";
+   //Si no existe
+    if (!($resultado = $this->mysqli->query($sql))){
+		return 'No existe'; 
+	}
+	//Devolucion de la consulta
+    else{ 
+		$result = $resultado;
+		return $result;
+	}
+}
+
+//Funcion para editar categoria
+function edit()
+{
+	//Sentencia sql que buscara todos los datos de una categoria
+    $sql = "SELECT * FROM categorias WHERE (`id_CATEGORIAS` = '$this->id_CATEGORIAS')";
+    
+    $result = $this->mysqli->query($sql);
+    
+	//Si devuelve 1a fila (consulta realizada correctamente)
+    if ($result->num_rows == 1)
+    {	
+		$sql = "UPDATE categorias SET		
+					`nombre` = '$this->nombre'				
+				WHERE (`id_CATEGORIAS` = '$this->id_CATEGORIAS')";
+				//Si se realiza erroneamente la edicion
+        if (!($resultado = $this->mysqli->query($sql))){
+			return 'Error en la modificación';
+		}
+		else{ 
+			//Edit correcto
+			return 'Modificado correctamente'; 
+		}
+    }//Si no encuentra ninguna categoria
+    else 
+		return 'No existe';
+}
+
+//Funcion sql que buscara las categorias que correspondan
+function search(){ 
+//Sentencia sql que buscara las categorias cuyo nombre contengan la cadena introducida en el form de SEARCH
+	     $sql = "SELECT *
+       			FROM categorias
+    			WHERE
+    				( 
+					(`id_CATEGORIAS` LIKE '%$this->id_CATEGORIAS%')	&&
+	 				(`nombre` LIKE '%$this->nombre%')
+									
+    				)";
+				
+   //Si se produce un error
+    if (!($resultado = $this->mysqli->query($sql))){
+		return 'Error en la búsqueda';
+		
+	}//Si las encuentra (aunque no devuelva ninguna)
+    else{ 
+		return $resultado;
+	}
+}
+
+//Devuelve una categoria
+function searchById(){ 
+	$sql = "SELECT *
+			  FROM categorias
+		   WHERE
+			   ( 
+			   
+				(`id_CATEGORIAS` LIKE '%$this->id_CATEGORIAS%')
+			   
+			   )";
+		//No la encuentra   
+	if (!($resultado = $this->mysqli->query($sql))){
+		return 'Error en la búsqueda';
+		
+	}
+	else{ //Busqueda positiva
+		return $resultado;
+	}
+}
+
+//Funcion de borrado de una categoria
+function delete()
+{	
+//Sentencia sql que buscara la categoria a borrar
+    $sql = "SELECT * FROM categorias WHERE (`id_CATEGORIAS` = '$this->id_CATEGORIAS')";
+    
+    $result = $this->mysqli->query($sql);
+    //Si se encuentra
+    if ($result->num_rows == 1)
+    {
+    	//Sentencia sql que borrara la categoria
+        $sql = "DELETE FROM categorias WHERE (`id_CATEGORIAS` = '$this->id_CATEGORIAS')";
+        
+		//Resultado positivo
+        if($this->mysqli->query($sql)){
+        
+			return 'Borrado correctamente';
+		}
+		else{//Si esta asociada a una tarea no se puede borrar
+			return 'No se puede borrar.Hay tareas asociadas a esta categoria';
+		}
+    } 
+    else//Si no existe
+        return 'No existe';
+} 
+
+
+ 
+}//fin de clase
+?> 
---
+++ Models/Fases_has_contactos_Model.php
@@ -0,0 +1,131 @@
+<!---MODELO DE LAS FASES_HAS_CONTACTOS
+ CREADO POR los Cangrejas EL 21/12/2018-->
+<?php
+
+class FASES_HAS_CONTACTOS_Model {
+
+	var $FASES_id_FASES;
+	var $FASES_TAREAS_id_TAREAS;
+    var $CONTACTOS_email;
+	
+	//Constructor de la clase
+	function __construct($FASES_id_FASES, $FASES_TAREAS_id_TAREAS, $CONTACTOS_email){
+
+		$this->FASES_id_FASES = $FASES_id_FASES;
+		$this->FASES_TAREAS_id_TAREAS = $FASES_TAREAS_id_TAREAS;
+		$this->CONTACTOS_email = $CONTACTOS_email;
+		
+		//Incluimos el archivo de acceso a la bd
+		include_once 'Access_DB.php';
+		//Funcion de conexion a la bd
+		$this->mysqli = ConnectDB();
+	}
+
+	//Funcion para añadir un contacto a una fase
+	function add(){
+					
+		$sql = "INSERT INTO fases_has_contactos
+				VALUES (
+					'$this->FASES_id_FASES',
+					'$this->FASES_TAREAS_id_TAREAS',
+					'$this->CONTACTOS_email'
+					)
+				";
+
+		if (!$this->mysqli->query($sql)) { 
+			return 'Error al insertar';		//Devuelve mensaje de error	
+		}else{ 
+		
+			return 'Insercion correcta'; //Devuelve mensaje de exito
+			
+		}
+
+	}
+
+	//Funcion que devuelve los contactos de una tarea
+	function getContactosOfTarea() {	
+		$sql = "SELECT * FROM fases_has_contactos WHERE (`FASES_TAREAS_id_TAREAS` = '$this->FASES_TAREAS_id_TAREAS') GROUP BY `CONTACTOS_email`";
+
+		if (!($resultado = $this->mysqli->query($sql))){
+			return 'No existe'; //Devuelve mensaje de error
+		}else{ 
+			$result = $resultado;//Se guarda el resultado de la consulta sql
+			return $result;//Se devuelve el resultado de la consulta
+		
+		}
+	}
+
+	//Funcion que devuelve los contactos de una fase
+	function getContactosOfFase() {	
+		$sql = "SELECT * FROM fases_has_contactos WHERE (`FASES_id_FASES` = '$this->FASES_id_FASES')";
+
+		if (!($resultado = $this->mysqli->query($sql))){
+			return 'No existe'; //Devuelve mensaje de error
+		}else{ 
+			$result = $resultado;//Se guarda el resultado de la consulta sql
+			return $result;//Se devuelve el resultado de la consulta
+		
+		}
+	}
+
+	//Funcion que busca los contactos de una fase
+	function search(){ 
+
+			$sql = "SELECT *
+					FROM fases_has_contactos
+					WHERE
+						( 
+						
+						(`FASES_id_FASES` LIKE '%$this->FASES_id_FASES%') &&
+						(`FASES_TAREAS_id_TAREAS` LIKE '%$this->FASES_TAREAS_id_TAREAS%') &&
+						(`CONTACTOS_email` LIKE '%$this->CONTACTOS_email%')
+						
+						)";
+					
+	
+		if (!($resultado = $this->mysqli->query($sql))){
+			return 'Error en la búsqueda';//Devuelve mensaje de error		
+		}else{ 
+			return $resultado;//Se devuelve el resultado de la consulta
+		}
+	}
+
+	//Funcion que borra un contacto de una fase
+	function delete(){	
+		$sql = "SELECT * FROM fases_has_contactos WHERE (`FASES_id_FASES` = '$this->FASES_id_FASES') &&
+						(`FASES_TAREAS_id_TAREAS` = '$this->FASES_TAREAS_id_TAREAS') &&
+						(`CONTACTOS_email` = '$this->CONTACTOS_email')";
+		
+		$result = $this->mysqli->query($sql);//Se guarda el resultado de la consulta sql
+		
+		if ($result->num_rows == 1){
+			
+			$sql = "DELETE FROM fases_has_contactos WHERE (`FASES_id_FASES` = '$this->FASES_id_FASES') &&
+						(`FASES_TAREAS_id_TAREAS` = '$this->FASES_TAREAS_id_TAREAS') &&
+						(`CONTACTOS_email` = '$this->CONTACTOS_email')";
+			
+			$this->mysqli->query($sql);
+			
+			return 'Borrado correctamente';//Devuelve mensaje de exito
+		}else{
+			return 'No existe';//Devuelve mensaje de error
+		}
+	}
+
+	//Funcion que devuelve los contactos de una fase
+	function rellenadatos() {	
+		$sql = "SELECT * FROM fases_has_contactos WHERE (`FASES_id_FASES` = '$this->FASES_id_FASES') &&
+						(`FASES_TAREAS_id_TAREAS` = '$this->FASES_TAREAS_id_TAREAS') &&
+						(`CONTACTOS_email` = '$this->CONTACTOS_email')";
+	
+		if (!($resultado = $this->mysqli->query($sql))){
+			return 'No existe'; //Devuelve mensaje de error
+		}else{ 
+			$result = $resultado;//Se guarda el resultado de la consulta sql
+			return $result;	//Se devuelve el resultado de la consulta
+		}
+	}
+
+}//fin de clase
+
+?> 
