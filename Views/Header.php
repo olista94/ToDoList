f4608939cc1a59771242b6012e6e8adf7c3742f0
Oscar Lista <43946678+olista94@users.noreply.github.com>
2019-10-08 18:35:37 +0200

    Menu deslizante

--- Views/Header.php
+++ Views/Header.php
@@ -3,7 +3,6 @@ CREADO POR: Los Cangrejas
 Fecha: 20/12/2018-->
 
 <?php
-
  //Comprueba si esta autenticado
 	include_once '../Functions/Authentication.php';
 	if (!isset($_SESSION['idioma'])) {
@@ -20,7 +19,7 @@ idioma = "<?php echo $_SESSION['idioma']; ?>"; //Variable global que permite pas
 
 <html>
 <head>
-	<title>ET4</title>
+	<title>TopPadel</title>
 	
 	 <!--Archivos css,js...necesarios para el funcionamiento de la aplicacion-->
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
@@ -35,7 +34,7 @@ idioma = "<?php echo $_SESSION['idioma']; ?>"; //Variable global que permite pas
 
 <?php
 if(isset($_SESSION['tipo'])){//Si se loguea como ADMIN
-	if($_SESSION['tipo']=='ADMIN'){
+	
 ?>
 
 <header id="main-header">	
@@ -43,7 +42,7 @@ if(isset($_SESSION['tipo'])){//Si se loguea como ADMIN
 	<div class="fixednav">
 		<div class="topnav">
 			<div class="topnav-centered">
-				<a><h2><?php echo $strings['ToDoList']; ?></h2></a>
+				<a><h2>TopPadel</h2></a>
 			</div>
 
 			<a class="alogo"><button class="logo"></button></a>
@@ -56,9 +55,23 @@ if(isset($_SESSION['tipo'])){//Si se loguea como ADMIN
                     if (IsAuthenticated()){//Si esta autenticado
                 ?>
 				 <!--Login del usuario conectado-->
-				<button class="user"><?php echo $_SESSION['login'];?></button>				   		
-				<!--Desconectar-->
-				<a href='../Functions/Desconectar.php'><button class="logout"></button></a>
+			<nav class="perfil"><ul>
+				<li><?php echo $_SESSION['login'];?></a>
+					
+					<ul><li>
+							<a href="../Controllers/Usuarios_Controller.php?action=Confirmar_SHOWCURRENT1"><?php echo $strings['Perfil']; ?></a>
+						</li>
+						<li>
+							<a href="../Controllers/Tareas_Controller.php?action=Mostrar_Completas"><?php echo $strings['Hacerte socio']; ?></a>
+						</li>
+						<li class="cerrar">
+							<a href='../Functions/Desconectar.php'><?php echo $strings['Cerrar sesion']; ?></a>
+						</li>
+					</ul>
+				</li>
+			</ul>
+			</nav>				
+				
 
                 <?php                    
                     }
@@ -92,19 +105,12 @@ if(isset($_SESSION['tipo'])){//Si se loguea como ADMIN
 	</div>
  <!--Opciones del menu que puede gestionar si es ADMIN (todo)-->
 	<div class="menu-bar" id="menu-bar">
-		<li><a class="dropdownElm" onclick="dropdown()"><?php echo $strings['Tareas']; ?></a>
-			<nav class="dropdownContent" id="myDropdown">
-				<a href="../Controllers/Tareas_Controller.php?action=default"><?php echo $strings['Todas']; ?></a>
-				<a href="../Controllers/Tareas_Controller.php?action=Mostrar_Completas"><?php echo $strings['Completas']; ?></a>
-				<a href="../Controllers/Tareas_Controller.php?action=Mostrar_NoCompletas"><?php echo $strings['Sin completar']; ?></a>
-			</nav>
-		</li>
-		<li><a href="../Controllers/Prioridades_Controller.php"><?php echo $strings['Prioridades']; ?></a></li>
+		
 		<li><a href="../Controllers/Usuarios_Controller.php"><?php echo $strings['Usuarios']; ?></a></li>
-		<li><a href="../Controllers/Contactos_Controller.php"><?php echo $strings['Contactos']; ?></a></li>
-		<li><a href="../Controllers/Categorias_Controller.php"><?php echo $strings['Categorias']; ?></a></li>
-		
-
+		<li><a href="../Controllers/Torneos_Controller.php"><?php echo $strings['Torneos']; ?></a></li>
+		<li><a href="../Controllers/Pistas_Controller.php"><?php echo $strings['Pistas']; ?></a></li>
+		<li><a href="../Controllers/Reservas_Controller.php"><?php echo $strings['Reservas']; ?></a></li>
+		<li><a href="../Controllers/Promociones_Controller.php"><?php echo $strings['Promociones']; ?></a></li>
 		
 		<a href="javascript:void(0);" class="icon" onclick="responsiveMenu()">
 			<i class="fa fa-bars"></i>
@@ -114,80 +120,5 @@ if(isset($_SESSION['tipo'])){//Si se loguea como ADMIN
 </header>
 
 <?php
-	}
-	else{//Si se loguea como un usuario normal
-		?>
-		
-		<header id="main-header">	
-
-	<div class="fixednav">
-		<div class="topnav">
-			<div class="topnav-centered">
-				<a><h2><?php echo $strings['ToDoList']; ?></h2></a>
-			</div>
-
-			<a class="alogo"><button class="logo"></button></a>
-
-			<div class="topnav-right">
-                <?php	
-                    if (IsAuthenticated()){//Si esta autenticado
-                ?>
-				 <!--Login del usuario conectado-->
-				<button class="user"><?php echo $_SESSION['login'];?></button>				   		
-				<!--Desconectar-->
-				<a href='../Functions/Desconectar.php'><button class="logout"></button></a>
-
-                <?php                    
-                    }
-                    else{
-                ?>
-				    <!--Si no esta logueado muestra un boton para ir al registro--> 
-                    <a href='../Controllers/Registro_Controller.php'><button class="registrar"></button></a>
-                <?php
-                    }	
-                ?>
-				
-			</div>
-<!--Banderas para cambiar el idioma-->
-		
-			<div class="flags1" >
-			 <!--Al ingles-->
-				<form name='idioma' action="../Functions/CambioIdioma.php" method="POST" style="display: inline-block; margin:0; padding:0;">
-					<input type="hidden" name='idioma' value="ENGLISH">
-					<input type="image" src="../Views/img/uk.png"  width="45px">
-				</form>
-				 <!--Al castellano-->
-				<form name='idioma' action="../Functions/CambioIdioma.php" method="POST" style="display: inline-block; margin:0; padding:0;">
-					<input type="hidden" name='idioma' value="SPANISH" >
-					<input type="image"  src="../Views/img/spain.png"  width="35px" >
-				</form>
-				 <!--Al gallego-->
-				<form name='idioma' action="../Functions/CambioIdioma.php" method="POST" style="display: inline-block; margin:0; padding:0;">
-					<input type="hidden" name='idioma' value="GALLAECIAN" >
-					<input type="image"  src="../Views/img/galicia.png" width="35px">	
-				</form>
-			</div>
-
-		</div>
-	</div>
- <!--Opciones del menu que puede gestionar si no es ADMIN (gestion de sus tareas y fases y añadir usuarios)-->
-	<div class="menu-bar" id="menu-bar">
-		<li><a class="dropdownElm" onclick="dropdown()"><?php echo $strings['Tareas']; ?></a>
-			<nav class="dropdownContent" id="myDropdown">
-				<a href="../Controllers/Tareas_Controller.php?action=default"><?php echo $strings['Todas']; ?></a>
-				<a href="../Controllers/Tareas_Controller.php?action=Mostrar_Completas"><?php echo $strings['Completas']; ?></a>
-				<a href="../Controllers/Tareas_Controller.php?action=Mostrar_NoCompletas"><?php echo $strings['Sin completar']; ?></a>
-			</nav>
-		</li>
-		<li><a href="../Controllers/Contactos_Controller.php"><?php echo $strings['Contactos']; ?></a></li>
-		<a href="javascript:void(0);" class="icon" onclick="responsiveMenu()">
-			<i class="fa fa-bars"></i>
-		</a>
-	</div>
-
-</header>
-		
-<?php
-}
 }
 ?>
